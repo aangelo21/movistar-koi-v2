455e57ff0b80e7928b04b7e771438366df4010c7
aangelo-21 <martinezglielmo@gmail.com>
2025-03-09 18:45:00 +0000

    finished home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,71 @@
+import "./Home.css";
+
 function Home() {
   return (
     <>
-      <p>Esto es Home</p>
+      <div class="home-container">
+        <div class="home-title-koi">
+          <h1>Movistar KOI</h1>
+        </div>
+        <div class="home-content-koi">
+          <p>
+            Movistar KOI tiene como objetivo dominar las competiciones más
+            prestigiosas, como la LEC y la Superliga de League of Legends.
+            Además, estamos comprometidos con la innovación y el uso responsable
+            de la tecnología, promoviendo iniciativas que fomenten un entorno
+            seguro y divertido para todos los jugadores. También nos esforzamos
+            por promover la diversidad e inclusión en el mundo del gaming,
+            apoyando proyectos que empoderen a las mujeres y fomenten un
+            ambiente respetuoso.
+          </p>
+          <br />
+          <p>
+            En cuanto a nuestros compromisos, nos esforzamos por ofrecer el
+            mejor rendimiento en cada competición, con equipos altamente
+            capacitados y apoyo técnico de vanguardia. Estamos dedicados a
+            fomentar un entorno seguro y respetuoso para todos los jugadores,
+            promoviendo valores como la inclusión y el respeto. Además, nos
+            comprometemos a innovar constantemente, manteniéndonos a la
+            vanguardia de las tendencias y tecnologías emergentes en el mundo
+            de los eSports.
+          </p>
+        </div>
+        <div class="home-image-koi">
+          <img src="other-images/movistar-koi-event.webp" alt="" />
+        </div>
+        <div class="home-title-fusion">
+          <h1>Fusion a 3 bandas</h1>
+        </div>
+        <div class="home-content-fusion">
+          <p>
+            MAD Lions, propiedad de OverActive Media, tiene una rica historia en
+            el mundo del eSports. Fundado originalmente como Splyce en 2017, el
+            equipo pasó por varias transformaciones hasta convertirse en MAD
+            Lions en 2019. Desde entonces, ha competido en torneos de League of
+            Legends y otros juegos, destacándose por su dinamismo y
+            competitividad. Por otro lado, Movistar Riders, apoyado por
+            Movistar, ha sido una fuerza emergente en el sector de los eSports
+            español desde su fundación en 2017. Con un enfoque en juegos como
+            League of Legends y Rainbow Six Siege, ha logrado establecerse como
+            una organización sólida y respetada en la escena europea.
+          </p>
+          <br />
+          <p>
+            Movistar KOI nació en enero de 2024 como resultado de la fusión
+            entre KOI, fundado por Ibai Llanos y Gerard Piqué, Movistar Riders,
+            y MAD Lions. Esta unión marca un hito en el mundo de los eSports,
+            combinando la pasión y el talento de tres equipos emblemáticos. Con
+            esta fusión, Movistar KOI compite en múltiples disciplinas,
+            incluyendo League of Legends, Valorant y Pokémon ofreciendo una
+            plataforma única para que los jugadores y aficionados disfruten de
+            la emoción de los eSports. ¡Únete a nuestra comunidad para seguir de
+            cerca nuestras aventuras y conquistas en este emocionante viaje!
+          </p>
+        </div>
+        <div class="home-image-fusion">
+          <img src="other-images/fusion.webp" alt="" />
+        </div>
+      </div>
     </>
   );
 }
