3a505941da6d7c16b5bd832407ab783c1cd21986
aangelo-21 <martinezglielmo@gmail.com>
2025-03-12 12:26:03 +0000

    added a few events

--- src/services/eventsData.js
+++ src/services/eventsData.js
@@ -1,14 +1,23 @@
 export const eventsData = [
   {
     id: '1',
-    title: 'Event 1',
-    start: '2025-03-16',
-    end: '2025-03-16',
+    title: 'FS vs TES',
+    start: '2025-03-12 10:00',
+    end: '2025-03-12 12:00',
+    description: 'Partido del first stand vs Top Esports \nResultado: Victoria 2-0'
   },
   {
     id: '2',
-    title: 'Event 2',
-    start: '2025-03-15',
-    end: '2025-03-15',
+    title: 'FS vs HLE',
+    start: '2025-03-13 10:00',
+    end: '2025-03-13 12:00',
+    description: 'Partido del first stand vs Hanwha Life \nResultado: Por jugar'
+  },
+  {
+    id: '2',
+    title: 'VCT vs TH',
+    start: '2025-03-15 17:00',
+    end: '2025-03-15 20:00',
+    description: 'Partido de VCT vs Team Heretics \nResultado: Por jugar'
   },
 ];
